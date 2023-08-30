74a4c238e41b6ea5c1625285ce354657ee62383e
Satyam Kumar <129041219+stkrfx@users.noreply.github.com>
2023-08-30 15:41:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -256,6 +256,5 @@ app.post('/user/:id',async(req,res)=>{
 })
 
 const port = process.env.PORT || 3001;
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.listen(port, () => 
+    console.log(`Server is running on http://localhost:${PORT}`));
